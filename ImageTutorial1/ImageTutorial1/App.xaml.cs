38a92f6ab1fb26273fa606e49f1d4743209c7609
Admin <Admin@DESKTOP-DUESHBO>
2021-12-27 02:39:59 +0300

    Добавьте файлы проекта.

---
+++ ImageTutorial1/ImageTutorial1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ImageTutorial1.Services;
+using ImageTutorial1.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImageTutorial1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
